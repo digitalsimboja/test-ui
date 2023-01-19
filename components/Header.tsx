f2375a65e40d8e64f7a84943f457bebd6b88c829
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-01-19 13:33:55 +0100

    sidebar

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="flex justify-between px-4 pt-4">
+            <h2>Projects</h2>
+            <h2>Cloud Platform</h2>
+            <h2> FLYTE</h2>
+        </div>
+    )
+}
+
+export default Header
