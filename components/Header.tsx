77bf2a9a216fba09f300562065da8ecc220289fa
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-01-19 14:41:09 +0100

    barchar and recent orders

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,10 @@
 const Header = () => {
     return (
-        <div className="flex justify-between px-4 pt-4">
-            <h2>Projects</h2>
-            <h2>Cloud Platform</h2>
-            <h2> FLYTE</h2>
-        </div>
+      <div className='flex justify-between px-4 pt-4'>
+          <h2>Dashboard</h2>
+          <h2>Welcome Back, Sunday</h2>
+      </div>
     )
-}
-
-export default Header
+  }
+  
+  export default Header
