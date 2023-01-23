adb54adb76d7b7f502e23054893d5064855b1c3e
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-01-23 07:33:05 +0100

    updates the avatars

--- components/FlyteOneCard.tsx
+++ components/FlyteOneCard.tsx
@@ -1,62 +1,71 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { RxAvatar } from "react-icons/rx";
 import {
-    TbBlur,
-    TbBrandGravatar,
-    TbCircleCheck,
-    TbCircleDotted,
-    TbCircleHalf,
-    TbDots,
-    TbPlus,
-  } from "react-icons/tb";
-  import { BsChatSquareDots } from "react-icons/bs";
+  TbBlur,
+  TbBrandGravatar,
+  TbCircleCheck,
+  TbCircleDotted,
+  TbCircleHalf,
+  TbDots,
+  TbPlus,
+} from "react-icons/tb";
+import { BsChatSquareDots } from "react-icons/bs";
 
 export const FlyteOneCard = () => {
   return (
     <div className="relative h-60 w-full">
-            <div className="absolute bottom-0 left-0 bg-gray-300 h-full w-full -z-5 rounded-lg "></div>
-            <div className="absolute left-2 bottom-2  bg-white h-full w-full z-10 transform rotate-3 rounded-lg">
-              <div className="p-2 absolute flex flex-col w-full">
-                <div className="flex justify-between">
-                  <h2>FLYTE-1</h2>
-                  <TbBrandGravatar size={20} className="mr-4" />
-                </div>
-                <h2 className="font-semibold mt-2 mb-1">UX Adjustimests</h2>
-                <p>
-                  Make UI/UX revisions for the project management dashboard on
-                  Figma.
-                </p>
-                <div className="flex justify-between mt-2">
-                  <div className="bg-blue-100 rounded-lg p-1 items-center text-blue-900 font-bold">
-                    <h2>Research</h2>
-                  </div>
-                  <div className="flex justify-end mr-2">
-                    <RxAvatar size={20} />
-                    <RxAvatar size={20} />
-                  </div>
-                </div>
-                <span className="border-b-[1px]  border-gray-200 w-full p-2 mb-4"></span>
-                <div className=" flex justify-between">
-                  <div className=" flex justify-start">
-                    <BsChatSquareDots className="rounded-sm" size={20} />
-                    <p className="font ml-2">3</p>
-                  </div>
-                  <div className="justify-end">
-                    <Image
-                      className="mr-8"
-                      width={23}
-                      height="31"
-                      src={"/images/hands.svg"}
-                      alt=""
-                    />
-                  </div>
-                </div>
+      <div className="absolute bottom-0 left-0 bg-gray-300 h-full w-full -z-5 rounded-lg "></div>
+      <div className="absolute left-2 bottom-2  bg-white h-full w-full z-10 transform rotate-3 rounded-lg">
+        <div className="p-2 absolute flex flex-col w-full">
+          <div className="flex justify-between">
+            <h2>FLYTE-1</h2>
+            <TbBrandGravatar size={20} className="mr-4" />
+          </div>
+          <h2 className="font-semibold mt-2 mb-1">UX Adjustimests</h2>
+          <p>
+            Make UI/UX revisions for the project management dashboard on Figma.
+          </p>
+          <div className="flex justify-between mt-2">
+            <div className="bg-blue-100 rounded-lg p-1 items-center text-blue-900 font-bold">
+              <h2>Research</h2>
+            </div>
+            <div className="flex justify-end mr-2">
+              <div className="relative rounded-full overflow-hidden z-0 -mr-4">
+                <img
+                  className="w-12 h-12 sm-w-16 sm-h-16 object-cover object-center"
+                  src="/images/Ellipse30.svg"
+                />
+              </div>
+              <div className="relative rounded-full overflow-hidden  sm:ml-0 z-10 -mr-4">
+                <img
+                  className="w-12 h-12 sm-w-16 sm-h-16 object-cover object-center"
+                  src="/images/Ellipse31.svg"
+                />
               </div>
             </div>
-            <div className="absolute bottom-0 left-2  right-2 bg-gray-200 h-full w-full z-5 transform rotate-3 rounded-lg"></div>
           </div>
-  )
-}
+          <span className="border-b-[1px]  border-gray-200 w-full p-2 mb-4"></span>
+          <div className=" flex justify-between">
+            <div className=" flex justify-start">
+              <BsChatSquareDots className="rounded-sm" size={20} />
+              <p className="font ml-2">3</p>
+            </div>
+            <div className="justify-end">
+              <Image
+                className="mr-8"
+                width={23}
+                height="31"
+                src={"/images/hands.svg"}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-2  right-2 bg-gray-200 h-full w-full z-5 transform rotate-3 rounded-lg"></div>
+    </div>
+  );
+};
 
-export default FlyteOneCard
+export default FlyteOneCard;
--- components/FlyteTwoCard.tsx
+++ components/FlyteTwoCard.tsx
@@ -1,45 +1,50 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { RxAvatar } from "react-icons/rx";
 import {
-    TbBlur,
-    TbBrandGravatar,
-    TbCircleCheck,
-    TbCircleDotted,
-    TbCircleHalf,
-    TbDots,
-    TbPlus,
-  } from "react-icons/tb";
-  import { BsChatSquareDots } from "react-icons/bs";
+  TbBlur,
+  TbBrandGravatar,
+  TbCircleCheck,
+  TbCircleDotted,
+  TbCircleHalf,
+  TbDots,
+  TbPlus,
+} from "react-icons/tb";
+import { BsChatSquareDots } from "react-icons/bs";
 
 export const FlyteTwoCard = () => {
   return (
     <div className="relative h-60 w-full mb-4 mt-4">
-            <div className="absolute bottom-0 left-0 bg-gray-200 h-full w-full rounded-lg border border-b-2"></div>
-            <div className="absolute left-0 bottom-2 p-2 bg-gray-50 h-full w-full z-10 rounded-lg border border-b-2">
-              <div className="p-2">
-                <h2 className="text-gray-500 ">FLYTE-2</h2>
-                <h2 className="font-semibold">Moodboards</h2>
-                <Image
-                  width={212}
-                  height={80}
-                  src={"/images/image2.png"}
-                  alt=""
+      <div className="absolute bottom-0 left-0 bg-gray-200 h-full w-full rounded-lg border border-b-2"></div>
+      <div className="absolute left-0 bottom-2 p-2 bg-gray-50 h-full w-full z-10 rounded-lg border border-b-2">
+        <div className="p-2">
+          <h2 className="text-gray-500 ">FLYTE-2</h2>
+          <h2 className="font-semibold">Moodboards</h2>
+          <Image width={212} height={80} src={"/images/image2.png"} alt="" />
+          <div className="flex justify-between">
+            <div className="items-center inline-block mt-2 p-2 bg-gray-200">
+              <h2 className="text-blue-500 font-semibold ">UI Design</h2>
+            </div>
+            <div className="flex justify-end mr-2 mt-2 p-2 items-center ">
+              <div className="relative rounded-full overflow-hidden z-0 -mr-4">
+                <img
+                  className="w-12 h-12 sm-w-16 sm-h-16 object-cover object-center"
+                  src="/images/avatar6.svg"
                 />
-                <div className="flex justify-between">
-                  <div className="items-center inline-block mt-2 p-2 bg-gray-200">
-                    <h2 className="text-blue-500 font-semibold ">UI Design</h2>
-                  </div>
-                  <div className="flex justify-end mr-2 mt-2 p-2 items-center ">
-                    <RxAvatar size={20} />
-                    <RxAvatar size={20} />
-                  </div>
-                </div>
               </div>
-              <span className="border-b-[1px]  border-gray-200 w-full p-2 mb-4"></span>
+              <div className="relative rounded-full overflow-hidden  sm:ml-0 z-10 -mr-4">
+                <img
+                  className="w-12 h-12 sm-w-16 sm-h-16 object-cover object-center"
+                  src="/images/avatar7.svg"
+                />
+              </div>
             </div>
           </div>
-  )
-}
+        </div>
+        <span className="border-b-[1px]  border-gray-200 w-full p-2 mb-4"></span>
+      </div>
+    </div>
+  );
+};
 
-export default FlyteTwoCard
+export default FlyteTwoCard;
